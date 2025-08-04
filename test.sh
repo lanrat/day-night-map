b4bb6da71dfc7909f2f78298b5c3954943a87a3a
Ian Foster <ian@vorsk.com>
2025-08-04 12:12:27 -0700

    try to get dithering working

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+RESOLUTION="800,480"
+
+google-chrome --headless --window-size=$RESOLUTION --screenshot="screenshot.png" "file://$SCRIPT_DIR/index.html"
+
+
+# magick screenshot.png -dither FloydSteinberg -remap pattern:gray50 -depth 1 -strip png:output.png
+#convert screenshot.png -monochrome -depth 3 -strip png:output.png
+
+docker run --rm -v "$SCRIPT_DIR:/app" -w /app --entrypoint magick minidocks/imagemagick \
+    screenshot.png -dither FloydSteinberg -remap pattern:gray50 -depth 1 -strip png:output.png
+
+# docker run --rm -v "$SCRIPT_DIR:/app" -w /app --entrypoint magick minidocks/imagemagick \
+#     screenshot.png -monochrome -colors 2 -depth 1 -strip png:output.png    
